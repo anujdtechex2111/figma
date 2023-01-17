b3f1aa960c52c3184dc5923eeb9509abe20d8cae
kamaldhami1010 <kdhami159@gmail.com>
2023-01-17 15:52:37 +0530

    layout change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import Home from "./page/Home";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Home />
+  </div>;
 }
 
 export default App;
---
+++ src/components/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopBar = (props) => {
+    return (
+        <div>
+
+            
+        </div>
+    );
+};
+
+export default TopBar;
