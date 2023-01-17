3b36479feae764d3bdcb0174c767ada545175dc3
anujdtechex2111 <anujjoshi.87210@gmail.com>
2023-01-17 18:24:28 +0530

    updated top

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import top from "../assets/images/top.png";
 const TopBar = (props) => {
-    return (
-        <div>
-
-            
-        </div>
-    );
+  return (
+    <div
+      className="h-screen p-8 relative "
+      style={{
+        background: `url(${top})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100% 100%",
+      }}
+    >
+      <div className="text-white absolute top-60 left-60  text-2xl  ">
+        WE ARE <br />
+        FACELESS
+        <p className="text-teal">Make secure payments seamlessly!</p>
+        <button className="rounded-full mt-4 text-base  text-justify text-white p-2 bg-teal">
+          LAUNCH APP
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+      teal: "#5eead4",
+    },
     extend: {},
   },
   plugins: [],
